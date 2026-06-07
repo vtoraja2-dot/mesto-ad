59ff3a95dfde8161b6943cd5254a93c6f1ec8e8b
vtoraja2-dot <vtoraja.2@gmail.com>
2026-06-07 07:48:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
-// Конфигурация сборщика Vite для проекта Mesto
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './',
-  base: '/mesto-production', // Относительные пути для корректной работы на GitHub Pages
+  base: '/mesto-production',
   server: {
     open: true,
     host: 'localhost',
